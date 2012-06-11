9577e836adafa728eaec0ebf34f62fbc1fb48b47
Miguel Mendes Ruiz <migmruiz@gmail.com>
2012-06-11 00:59:09 -0300

    Set music directory using a browse button and simplify some aspects of the SWT implementation class

--- src/br/lyfi/ui/LyricsFinderSWT_UI.java
+++ src/br/lyfi/ui/LyricsFinderSWT_UI.java
@@ -1,10 +1,10 @@
 package br.lyfi.ui;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
@@ -12,6 +12,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
@@ -26,30 +27,34 @@ import br.lyfi.LyricsFinder;
  */
 public class LyricsFinderSWT_UI extends Composite {
 
-	Text dataDirField;
-	Text lyricsField;
-	Text artistField;
+	Text _dataDirField;
+	Text _lyricsField;
+	Text _artistField;
+
+	Button _dataDirBrowse;
+
+	DirectoryDialog _dataDirFileDialog;
 
 	List<Text> fields; // all fields
 
-	public LyricsFinderSWT_UI(Composite parent) {
-		this(parent, SWT.NONE);
+	Button _findButton;
+	Button _clearButton;
+
+	public LyricsFinderSWT_UI(Shell shell) {
+		this(shell, SWT.NONE);
 	}
 
-	public LyricsFinderSWT_UI(Composite parent, int style) {
-		super(parent, style);
+	public LyricsFinderSWT_UI(Shell shell, int style) {
+		super(shell, style);
 		fields = new ArrayList<Text>();
+		_dataDirFileDialog = new DirectoryDialog(shell, SWT.OPEN);
 		createGui();
 	}
 
 	protected void clearFields() {
-		for (Iterator<Text> i = fields.iterator(); i.hasNext();) {
-			((Text) i.next()).setText("");
+		for (Text field : fields) {
+			field.setText("");
 		}
-	}
-
-	protected Text createLabelledText(Composite parent, String label) {
-		return createLabelledText(parent, label, 20, null);
 	}
 
 	protected Text createLabelledText(Composite parent, String label,
@@ -68,11 +73,6 @@ public class LyricsFinderSWT_UI extends Composite {
 		return text;
 	}
 
-	protected Button createButton(Composite parent, String label,
-			SelectionListener l) {
-		return createButton(parent, label, l);
-	}
-
 	protected Button createButton(Composite parent, String label, String tip,
 			SelectionListener l) {
 		Button b = new Button(parent, SWT.NONE);
@@ -86,76 +86,100 @@ public class LyricsFinderSWT_UI extends Composite {
 		return b;
 	}
 
-	// partial selection listener
-	private class MySelectionAdapter implements SelectionListener {
-		public void widgetSelected(SelectionEvent e) {
-			// default is to do nothing
-		}
-
-		public void widgetDefaultSelected(SelectionEvent e) {
-			widgetSelected(e);
-		}
-	};
-
 	protected void createGui() {
+
 		setLayout(new GridLayout(1, true));
 
-		// create the input area
+		// create the setup area
 
 		Group entryGroup = new Group(this, SWT.NONE);
-		entryGroup.setText("Input Values");
+		entryGroup.setText("Setup");
 		// use 2 columns, not same width
 		GridLayout entryLayout = new GridLayout(2, false);
 		entryGroup.setLayout(entryLayout);
 		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		gridData.widthHint = 400;
 		entryGroup.setLayoutData(gridData);
+		// make all buttons the same size
+		FillLayout buttonLayout = new FillLayout();
+		buttonLayout.marginHeight = 2;
+		buttonLayout.marginWidth = 2;
+		buttonLayout.spacing = 5;
+		Composite browseButton = new Composite(this, SWT.NONE);
+		browseButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+				false));
+		browseButton.setLayout(buttonLayout);
 
-		dataDirField = createLabelledText(entryGroup, "Data directory: ", 80,
-				"Enter the data directory path");
-		// TODO Set music directory using a browse button
-		lyricsField = createLabelledText(entryGroup, "Partial lyrics: ", 40,
+		_dataDirField = createLabelledText(entryGroup, "Data directory: ", 80,
+				"Enter the data directory path"
+						+ " or use the browse button below");
+
+		_dataDirBrowse = createButton(browseButton, "&Browse",
+				"Browse and select" + " the audio library directory",
+				new SelectionAdapter() {
+					public void widgetSelected(SelectionEvent e) {
+						String filename = _dataDirFileDialog.open();
+						if (filename != null) {
+							_dataDirField.setText(filename);
+						}
+					}
+				});
+
+		// create the input area
+
+		entryGroup = new Group(this, SWT.NONE);
+		entryGroup.setText("Input Values");
+		entryGroup.setLayout(entryLayout);
+		gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gridData.widthHint = 400;
+		entryGroup.setLayoutData(gridData);
+
+		_lyricsField = createLabelledText(entryGroup, "Partial lyrics: ", 40,
 				"Enter the partial lyrics");
-		artistField = createLabelledText(entryGroup, "Artist: ", 20,
+		_artistField = createLabelledText(entryGroup, "Artist: ", 20,
 				"Enter the artist name, if you remember");
 
 		// create the button area
 
 		Composite buttons = new Composite(this, SWT.NONE);
 		buttons.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		// make all buttons the same size
-		FillLayout buttonLayout = new FillLayout();
-		buttonLayout.marginHeight = 2;
-		buttonLayout.marginWidth = 2;
-		buttonLayout.spacing = 5;
+		// set all buttons the same size
 		buttons.setLayout(buttonLayout);
 
-		@SuppressWarnings("unused")
-		Button findButton = createButton(buttons, "&Find",
-				"Process input and find" + " the matching audio file",
-				new MySelectionAdapter() {
-					public void widgetSelected(SelectionEvent e) {
-						// TODO graphical output
-						System.out.println("Data directory path:         "
-								+ dataDirField.getText());
-						System.out.println("Partial lyrics:         "
-								+ lyricsField.getText());
-						System.out.println("Artist:      "
-								+ artistField.getText());
-						doFind(dataDirField.getText(), lyricsField.getText(), artistField.getText());
-					}
-				});
+		_findButton = createButton(buttons, "&Find", "Process input and find"
+				+ " the matching audio file", new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				// TODO graphical output
+				System.out.println("Data directory path:         "
+						+ _dataDirField.getText());
+				System.out.println("Partial lyrics:         "
+						+ _lyricsField.getText());
+				System.out.println("Artist:      " + _artistField.getText());
+				if (validate()) {
+					doFind(_dataDirField.getText(), _lyricsField.getText(),
+							_artistField.getText());
+				}
+			}
+		});
 
-		@SuppressWarnings("unused")
-		Button clearButton = createButton(buttons, "&Clear", "clear inputs",
-				new MySelectionAdapter() {
+		_clearButton = createButton(buttons, "&Clear", "clear inputs",
+				new SelectionAdapter() {
 					public void widgetSelected(SelectionEvent e) {
 						clearFields();
-						lyricsField.forceFocus();
+						_lyricsField.forceFocus();
 					}
 				});
 	}
-	
+
+	protected boolean validate() {
+		boolean valid = false;
+		if (!(_dataDirField.getText() == null || _lyricsField.getText() == null)) {
+			valid = !(_dataDirField.getText().isEmpty() || _lyricsField
+					.getText().isEmpty());
+		}
+		return valid;
+	}
+
 	private void doFind(String dataDirPath, String lyricsExp, String artist) {
 
 		// TODO Use given artist
